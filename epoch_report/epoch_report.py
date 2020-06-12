667acc22770f2d99552dcbbe2aa015afd3b464c6
joaquintides <joaquin@tid.es>
2020-06-12 18:33:32 +0200

    better handling of Github header anchors, added lax mode

--- epoch_report/epoch_report.py
+++ epoch_report/epoch_report.py
@@ -10,10 +10,13 @@
 import argparse
 import json
 import os
+import re
 import sys
 
 parser=argparse.ArgumentParser(
   description="Boost epoch report generator")
+parser.add_argument(
+  "-l","--lax",action="store_true",help="lax mode")
 parser.add_argument(
   "cdmap_file",metavar="<cdmap-file>",
   help="path to JSON file with Boost conditional dependency map")
@@ -22,6 +25,7 @@ parser.add_argument(
   help="path to JSON file with info on Boost libs explicitly banned for epoch progression")
 args=parser.parse_args()
 
+lax_mode=args.lax
 cdmap_file=args.cdmap_file
 if not os.path.exists(cdmap_file):
   sys.stderr.write("Can't find "+cdmap_file+"\n")
@@ -39,19 +43,25 @@ epochs={module:None for module in modules}
 block=dict()
 assessment_done={module:False for module in modules}
 
-def markup(module): return "[`{}`](#{})".format(
-  module,module.replace(" ","-").replace("/",""))
 
+# https://github.com/jch/html-pipeline/blob/master/lib/html/pipeline/toc_filter.rb
+forbidden_link_chars=re.compile(r"[^\w\- ]")
+
+def markup(module): return "[`{}`](#{})".format(
+  module,forbidden_link_chars.sub("",module.lower()).replace(" ","-"))
+
+def add_block(module,epoch,dep):
+  if not module in block:
+    block[module]={"epoch":epoch,"reason":"Depends on {}".format(markup(dep))}
+  else:
+    block[module]["reason"]+=", {}".format(markup(dep))
+  
 def assess_dependencies(module,epoch,cyclic_deps):
   for dep in sorted(set(cdmap[module][epoch])-cyclic_deps):
-    assess(dep,cyclic_deps|{dep})
-    if dep in block and block[dep]["epoch"]<=epoch:
-      if not module in block:
-        block[module]={"epoch":epoch,"reason":"Depends on {}".format(markup(dep))}
-      else:
-        block[module]["reason"]+=", {}".format(markup(dep))
+    assess_strict(dep,cyclic_deps|{dep})
+    if dep in block and block[dep]["epoch"]<=epoch: add_block(module,epoch,dep)
   
-def assess(module,cyclic_deps=set()):
+def assess_strict(module,cyclic_deps=set()):
   if not assessment_done[module]:
     epochs[module]=[]
     block.pop(module,None)
@@ -67,6 +77,22 @@ def assess(module,cyclic_deps=set()):
     set().union(*(set(deps) for deps in cdmap[module].values()))):
     assessment_done[module]=True
 
+def assess_lax(module):
+  epochs[module]=[]
+  for epoch in sorted(cdmap[module].keys()):
+    if module in ban and ban[module]["epoch"]==epoch:
+      block[module]=ban[module]
+      break
+    else:
+      for dep in cdmap[module][epoch]:
+        if dep in ban and ban[dep]["epoch"]<=epoch: add_block(module,epoch,dep)
+      if module in block: break
+      epochs[module].append(epoch)
+
+def assess(module):
+  if lax_mode: assess_lax(module)
+  else: assess_strict(module)
+  
 for module in modules: assess(module)
 
 sys.stdout.write("## Boost modules\n\n")
